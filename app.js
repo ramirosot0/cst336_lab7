60b377205bab2d726f091844044c83f07bbb8452
Ramiro Soto <tkokoloko@gmail.com>
2019-11-01 01:58:39 -0700

    Initial commit of Planets site

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+//reoutes
+app.get("/", function (req, res) {
+   res.render("index.html");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/mercury", function (req, res) {
+   res.render("mercury.html");
 });
 
-module.exports = app;
+app.get("/venus", function (req, res) {
+    res.render("venus.html");
+});
+
+app.get("/earth", function (req, res) {
+    res.render("earth.html");
+});
+//server listener
+// app.listen("8081", "127.0.0.1", function () {
+//    console.log("Express server is running...");
+// });
+
+//heroku listener
+app.listen(process.env.PORT, process.env.IP, function () {
+    console.log("Running Express Server...");
+});
